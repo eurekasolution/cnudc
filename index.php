275b52b3c7b460e3b20231e686896c0b789db4b6
eurekasolution <kjmin@daum.net>
2024-03-07 07:56:34 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>충남대학교 한문학과 예제</title>
+</head>
+<body>
+    <h1>충남대학교 한문학과</h1>
+    <p>한문학과는 충남대학교의          aaa
+        
+    <br><br><br>
+    
+    
+    
+    한국문학과 관련된 과목을 다룹니다.</p>
+</body>
+</html>
